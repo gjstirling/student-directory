2a70de245b3e3d1d5c88374602ffc670cb300e41
Graeme Stirling <graemejstirling@gmail.com>
2021-07-20 12:26:10 +0100

    Ex5

--- directory.rb
+++ directory.rb
@@ -7,7 +7,11 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do 
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    puts "Add their favourite hobby"
+    hobby = gets.chomp
+    puts "enter their height"
+    height = gets.chomp
+    students << {name: name, cohort: :november, hobby: hobby, height: height}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
@@ -23,7 +27,7 @@ end
 def print(students)
   count = 0 
   while true do 
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{students[count][:name]}, Height:#{students[count][:height]}, Hobby:#{students[count][:hobby]}, (#{students[count][:cohort]} cohort)"
     count += 1 
     break if count == students.length
   end
