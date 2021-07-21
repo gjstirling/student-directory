1d70e0cf5faf8f4f2558e62c041a0a3a04a75a72
Graeme Stirling <graemejstirling@gmail.com>
2021-07-21 11:11:48 +0100

    Step 10

--- directory.rb
+++ directory.rb
@@ -1,34 +1,44 @@
 def interactive_menu
-  students = []
+  @students = []
   loop do 
-    # 1. print the menu and ask the user what to complete
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
      # 2. read the input and save it into a variable
-    selection = gets.chomp
+    process(gets.chomp)
     # 3. do what the user has asked
-    case selection 
-    when "1"
-      students = input_students
-    when "2"
-      print(students)
-      print_footer(students)
-      # show the students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end 
   end
 end
+
+def process(selection)
+   case selection 
+   when "1"
+     input_students
+   when "2"
+     show_students
+     # show the students
+   when "9"
+     exit # this will cause the program to terminate
+   else
+     puts "I don't know what you meant, try again"
+   end
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to complete
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print()
+  print_footer()
+end 
 
 
 
 def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -41,13 +51,13 @@ def input_students
     hobby = gets.chomp
     puts "enter their height"
     height = gets.chomp
-    students << {name: name, cohort: cohort, hobby: hobby, height: height}
-    puts "Now we have #{students.count} student" if students.count == 1
-    puts "Now we have #{students.count} students" if students.count > 1
+    @students << {name: name, cohort: cohort, hobby: hobby, height: height}
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count > 1
     # get another name from the user
     name = gets.chomp
   end 
-students 
+@students 
 end
 
 def print_header 
@@ -55,21 +65,24 @@ def print_header
   puts "-------------".center(80)
 end
 
-def print(students)
-  return if students.length == 0 
+def print
+  return if @students.length == 0 
   puts "Which cohort would you like to print?"
   cohort = gets.chomp
   print_header
-  students.each_with_index do |student, index|
+  @students.each_with_index do |student, index|
     if student[:cohort] == cohort
     puts "#{student[:name].center(20)} Height:#{student[:height].center(20)} Hobby:#{student[:hobby].center(20)}"
     end
   end
 end
 
-def print_footer(names)
-  puts "No students entered"; return if names.length == 0 
-  puts "Overall, we have #{names.count} great students".center(80)
+def print_footer
+  if @students.length == 0  
+    puts "No students entered" 
+  else
+    puts "Overall, we have #{@students.count} great students".center(80)
+  end
 end 
 
 interactive_menu
