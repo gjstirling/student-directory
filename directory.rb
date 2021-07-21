1696d71e24fd5d905fd43799ffe6b567bbc72168
Graeme Stirling <graemejstirling@gmail.com>
2021-07-21 14:49:03 +0100

    Step 14, Ex1

--- directory.rb
+++ directory.rb
@@ -9,6 +9,24 @@ def interactive_menu
   end
 end
 
+def process(selection)
+  case selection 
+  when "1"
+   input_students
+  when "2"
+   show_students
+    # show the students
+  when "3" 
+   save_students
+  when "4"
+   load_students
+  when "9"
+   exit # this will cause the program to terminate
+  else
+   puts "I don't know what you meant, try again"
+  end
+end
+
 def try_load_students
   filename = ARGV.first 
   return if filename.nil?
@@ -25,9 +43,32 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    data(name, cohort)
   end
   file.close
+end
+
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do 
+    # add the student hash to the array
+    puts "Which cohort are they joining ?"
+    cohort = STDIN.gets.delete_suffix!("\n")
+    data(name, cohort)
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count > 1
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end 
+@students 
+end
+
+def data(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def save_students 
@@ -43,24 +84,6 @@ def save_students
    file.close
  end
 
-def process(selection)
-   case selection 
-   when "1"
-    input_students
-   when "2"
-    show_students
-     # show the students
-   when "3" 
-    save_students
-   when "4"
-    load_students
-   when "9"
-    exit # this will cause the program to terminate
-   else
-    puts "I don't know what you meant, try again"
-   end
-end
-
 def print_menu
   # 1. print the menu and ask the user what to complete
   puts "1. Input the students"
@@ -74,25 +97,6 @@ def show_students
   print
   print_footer()
 end 
-
-def input_students 
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do 
-    # add the student hash to the array
-    puts "Which cohort are they joining ?"
-    cohort = STDIN.gets.delete_suffix!("\n")
-    @students << {name: name, cohort: cohort}
-    puts "Now we have #{@students.count} student" if @students.count == 1
-    puts "Now we have #{@students.count} students" if @students.count > 1
-    # get another name from the user
-    name = gets.chomp
-  end 
-@students 
-end
 
 def print_header 
   puts "The students of Villains Academy".center(80)
