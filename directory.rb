8b3babea0501315ef4adcbadde27626c26925c48
Graeme Stirling <graemejstirling@gmail.com>
2021-07-21 10:18:54 +0100

    Interactive menu step 9

--- directory.rb
+++ directory.rb
@@ -1,3 +1,30 @@
+def interactive_menu
+  students = []
+  loop do 
+    # 1. print the menu and ask the user what to complete
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+     # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection 
+    when "1"
+      students = input_students
+    when "2"
+      print(students)
+      print_footer(students)
+      # show the students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end 
+  end
+end
+
+
+
 def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -9,7 +36,6 @@ def input_students
     # add the student hash to the array
     puts "Which cohort are they joining ?"
     cohort = gets .delete_suffix!("\n")
-    puts cohort
     cohort = "None specified" if cohort == ""
     puts "Add their favourite hobby"
     hobby = gets.chomp
@@ -46,6 +72,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(80)
 end 
 
-students = input_students
-print(students)
-print_footer(students)
+interactive_menu
